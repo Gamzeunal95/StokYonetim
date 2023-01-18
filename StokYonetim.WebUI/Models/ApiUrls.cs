d8853c308c7be771af0a1b2931ea51a41c69382b
gamzeunal95 <gamzeunal9505@gmail.com>
2023-01-18 18:00:32 +0300

    ApiUrl revize

--- StokYonetim.WebUI/Models/ApiUrls.cs
+++ StokYonetim.WebUI/Models/ApiUrls.cs
@@ -5,10 +5,16 @@
         public string GetById { get; set; }
         public string GetAll { get; set; }
         public string Login { get; set; }
+        public string Post { get; set; }
+        public string Put { get; set; }
+        public string Delete { get; set; }
         public ApiUrls(string TableName)
         {
             GetById = "api/" + TableName + "/GetById/";
             GetAll = "api/" + TableName + "/GetAll/";
+            Post = "api/" + TableName + "/Post/";
+            Put = "api/" + TableName + "/Put/";
+            Delete = "api/" + TableName + "/Delete/";
             Login = "api/Login/Login/";
         }
     }
